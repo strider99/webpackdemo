9616be7efe395ead889363896a35383137c207ef
strider99 <panigrahiomprakash@gmail.com>
2019-01-13 06:47:42 +0530

    create folder structure and dummy code

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom';
+import Users from './containers/Users';
+import Pizza from './containers/Pizza';
+import asyncComponent from './hoc/asyncComponent';
+
+const AsyncPizza = asyncComponent(() => {
+  return import('./containers/Pizza.js');
+})
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to='/'>Users</Link>
+          <Link to='/pizaa'>Pizza</Link>
+        </div>
+        <div>
+          <Route path='/' exact component={Users} />
+          <Route path='/pizza' exact component={AsyncPizza} />
+          
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+  render(){
+    return (
+     <div>
+       <h1>The Users</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, commodi.</p>
+     </div>
+    );
+  }
+}
+
+
+export default Users;
