390a539f94198784e8b911e0a02b09e9ffc9c5cd
strider99 <panigrahiomprakash@gmail.com>
2019-01-13 07:33:10 +0530

    setting up the basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output : {
+    path: '',
+    filename: 'bundle.js',
+    publicPath: path.resolve(__dirnname, 'dist')
+  }
+
+};
