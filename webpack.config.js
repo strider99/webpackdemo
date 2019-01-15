ba34fe94c30bdb71addd6a6fcbfe491d669fedad
strider99 <panigrahiomprakash@gmail.com>
2019-01-15 05:43:14 +0530

    creating rules for images

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = {
           }
         ]
 
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
       }
     ]
   }
